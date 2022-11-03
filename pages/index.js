23cbcec003618b54052ed26849d19749de7d910d
StR0vErBG <xcreaw1@abv.bg>
2022-11-03 04:36:06 +0300

    just to commit other things :D

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/legacy/image";
 import HeroSection from "../components/indexPage/HeroSection";
 import Navigation from "../components/indexPage/Navigation";
 
@@ -15,33 +16,48 @@ export default function Home() {
 
       <main className="mt-5">
         <HeroSection />
-        <section className="container text-center">
-          <div>
-            <h5 className="text-primaryBlue-450">КОИ СМЕ НИЕ?</h5>
-            <p className="text-white">
+        <section className="container text-center flex flex-col relative mt-64 ">
+          <div className="flex-center flex-col mr-10 absolute">
+            <h5 className="text-primaryBlue-450 text-3xl font-medium mb-2">
+              КОИ СМЕ НИЕ?
+            </h5>
+            <p className="text-white max-w-xl">
               Ние сме Strover, млади, амбициозни и креативни интернет паяци,
               които са готови да се учат от всичко и всички. Плетем мрежата си
               около онлайн търговията, уеб дизайна, онлайн маркетинга и SEO
               оптимизацията, насочени към съществуващи и нови бизнеси.
             </p>
           </div>
-          <div>
-            <h5 className="text-primaryBlue-450">КАКВО ЩЕ ПОЛУЧИТЕ?</h5>
-            <p className="text-white">
+          <div className="flex-center flex-col absolute">
+            <h5 className="text-primaryBlue-450 text-3xl font-medium mb-2">
+              КАКВО ЩЕ ПОЛУЧИТЕ?
+            </h5>
+            <p className="text-white max-w-xl">
               Комплексна услуга от хора, които са специалисти в съответните
               сфери: дигиталния маркетинг, компютърните науки и онлайн
               продажбите. Ние ще Ви предоставим цялостни дигитални решения за
               развитието на бранда Ви и достигането му до ИДЕАЛНИТЕ КЛИЕНТИ.
             </p>
           </div>
-          <div>
-            <h5 className="text-primaryBlue-450">ЗАЩО СМЕ РАЗЛИЧНИ?</h5>
-            <p className="text-white">
+          <div className="flex-center flex-col absolute">
+            <h5 className="text-primaryBlue-450 text-3xl font-medium mb-2">
+              ЗАЩО СМЕ РАЗЛИЧНИ?
+            </h5>
+            <p className="text-white max-w-xl">
               Защото държим на индивидуалния подход към клиента, както и на
               добрата комуникация по всеки един проект. Когато за него няма
               нужда от сложни технически термини - просто не ги използваме. За
               нас визията в интернет пространството е изключително важна.
             </p>
+          </div>
+          <div className="absolute death-center top-96">
+            <div className="relative w-[1500px] h-[800px]">
+              <Image
+                alt="line"
+                src="/indexForms/lines/linesTest.svg"
+                layout="fill"
+              />
+            </div>
           </div>
         </section>
       </main>
