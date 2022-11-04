c155e0b1ba00d8864689474443aaa509f4b053b4
StR0vErBG <xcreaw1@abv.bg>
2022-11-04 14:01:20 +0300

    dynamic layout based on router

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Navigation from "../indexPage/Navigation";
 
 import Footer from "./Footer";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
+  const router = useRouter();
+  const [bgColor, setBgColor] = useState();
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.removeAttribute("class");
+
+    if (router.pathname == "/") {
+      body.classList.add("index-bg");
+    } else if (router.pathname == "/digital") {
+      body.classList.add("digital-bg");
+    }
+  }, [router]);
   return (
-    <div className="flex flex-col justify-between min-h-screen">
+    <div
+      className={`flex flex-col justify-between min-h-screen h-full ${bgColor}`}
+    >
       {children}
       <Footer />
     </div>
