b9ced9779ec49416b8a82e881e656c472d3705cf
StR0vErBG <xcreaw1@abv.bg>
2022-11-12 01:27:31 +0300

    again testing :D

--- components/indexPage/AboutUs.js
+++ components/indexPage/AboutUs.js
@@ -40,7 +40,7 @@ export default function AboutUs() {
           <h5 className="mb-2 text-xl md:text-3xl font-medium text-primaryBlue-450">
             КОИ СМЕ НИЕ?
           </h5>
-          <p className="max-w-xl max-md:container text-lg leading-8 text-white max-md:text-justify max-md:hypens-auto ">
+          <p className="max-w-xl max-md:container text-lg leading-8 text-white max-md:text-justify hypens-auto ">
             Ние сме Strover, млади, амбициозни и креативни интернет паяци, които
             са готови да се учат от всичко и всички. Плетем мрежата си около
             онлайн търговията, уеб дизайна, онлайн маркетинга и SEO
@@ -57,7 +57,7 @@ export default function AboutUs() {
             <h5 className="mb-2 text-xl md:text-3xl font-medium text-primaryBlue-450">
               КАКВО ЩЕ ПОЛУЧИТЕ?
             </h5>
-            <p className="max-w-xl max-md:container text-lg leading-8 text-white max-md:hypens-auto max-md:text-justify">
+            <p className="max-w-xl max-md:container text-lg leading-8 text-white hypens-auto max-md:text-justify">
               Комплексна услуга от хора, които са специалисти в съответните
               сфери: дигиталния маркетинг, компютърните науки и онлайн
               продажбите. Ние ще Ви предоставим цялостни дигитални решения за
@@ -75,7 +75,7 @@ export default function AboutUs() {
             <h5 className="mb-2 text-xl md:text-3xl font-medium text-primaryBlue-450">
               ЗАЩО СМЕ РАЗЛИЧНИ?
             </h5>
-            <p className="max-w-xl text-lg  max-md:container leading-8 text-white max-md:text-justify max-md:hypens-auto">
+            <p className="max-w-xl text-lg  max-md:container leading-8 text-white max-md:text-justify hypens-auto">
               Защото държим на индивидуалния подход към клиента, както и на
               добрата комуникация по всеки един проект. Когато за него няма
               нужда от сложни технически термини - просто не ги използваме. За
