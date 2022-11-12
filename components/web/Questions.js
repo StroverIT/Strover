983b2fe36eca47430b25be2cee2081fca2b134e2
StR0vErBG <xcreaw1@abv.bg>
2022-11-12 18:37:21 +0300

    separation of concerns, animations and adjustments for mobile

--- components/web/Questions.js
+++ components/web/Questions.js
@@ -8,34 +8,9 @@ import { motion, AnimatePresence } from "framer-motion";
 import TextAnimation from "../../libs/TextAnimation";
 // Icons
 import { AiOutlinePlus } from "react-icons/ai";
-const questionData = [
-  {
-    title: "КОЛКО ПАРИ ЩЕ МИ СТРУВА",
-    text: `Всеки наш проект има индивидуален дизайн и бизнес логика. Офертата, която ще Ви предложим за изработката на уеб сайт или онлайн магазин ще бъде специално създадена за вас като съотношение цена-качество  и ще е съобразена спрямо целите, които гоните, функционалностите и мащаба на сайта`,
-    _id: "1231231212",
-  },
-  {
-    title: "КАКВО ОЧАКВАТЕ ОТ МЕН",
-    text: `От Вас очакваме да ни предоставите информацията, с която ще искате да представите бизнеса си, дейността си, продуктите и/или услугите си. Разбира се, нашият екип ще Ви съдейства с предложения, препоръки и подробни насоки. Ако нямате времето за тази подготовка, то ще може да Ви съдействаме и с този етап.`,
-    _id: "1231311",
-  },
-  {
-    title: "ИМАМ ПО-ИЗГОДНИ ПРЕДЛОЖЕНИЯ",
-    text: `Добре разработеният сайт генерира приходи от онлайн пространството Ви години наред. Професионалният ни съвет е да не се фокусирате върху разлики в рамките на няколкостотин лева, които се възвръщат моментално като инвестиция, а да съпоставяте предложенията по това какво ще донесат за бизнеса Ви.`,
-    _id: "21312323112",
-  },
-  {
-    title: "КОЛКО ВАРИАНТА НА ВИЗИИ ЩЕ МИ ДАДЕТЕ",
-    text: `Ние не работим по готови темплейти, затова Ви предоставяме индивидуални дизайни, които са съобразени с вашите предпочитания за визия. Те се съчетаваст с успешни работещи практити, за да Ви предоставим  най-добрите решения. Разбира се първо минава през Вашето одобрение и корекции при необходимост. `,
-    _id: "sadc2sa",
-  },
-  {
-    title: "КАК КЛИЕНТИТЕ ЩЕ ДОСТИГАТ ДО МЕН",
-    text: `Въжмосностите са безкрайни затова избираме най-оптималният начин, спрямо вашият бизнес и технологии за изграждането на сайта.`,
-    isBorder: false,
-    _id: "1231sada211s",
-  },
-];
+import { questionData } from "./questionComp/questionData";
+import BlueLine from "./questionComp/BlueLine";
+
 export default function Questions() {
   return (
     <section>
@@ -52,10 +27,13 @@ export default function Questions() {
         </div>
       </div>
 
-      <section className="relative w-full bg-blue-200 mt-28 pb-36">
+      <section className="relative w-full bg-blue-200 mt-28 pb-36  ">
         <section className="container pt-28">
           <div className="flex-col flex-center">
-            <div className="text-4xl font-semibold text-blue-50">
+            <div
+              className="text-4xl font-semibold text-blue-50 hidden lg:flex"
+              id="questions"
+            >
               <TextAnimation
                 placeholderText={[
                   {
@@ -65,7 +43,14 @@ export default function Questions() {
                 ]}
               />
             </div>
-            <p className="max-w-lg mt-1 text-center">
+
+            <div
+              className="text-xl lg:text-4xl font-semibold text-blue-50  max-lg:text-center lg:hidden"
+              id="questions"
+            >
+              ЧЕСТО ЗАДАВАНИ ВЪПРОСИ
+            </div>
+            <p className="max-lg:text-sm max-w-lg mt-1 text-center ">
               Какви са най-често задаваните въпроси, които вълнуват нашите
               клиенти преди да започнем съвместната си работа
             </p>
@@ -73,12 +58,8 @@ export default function Questions() {
         </section>
 
         {/* Questions */}
-        <section className="relative z-20 flex flex-col mx-auto mt-10 lg:px-48 max-w-7xl">
-          <div className="flex-center">
-            <div className="w-4 h-4 rounded-full bg-blue-250"></div>
-            <div className="w-full h-1 bg-blue-250"></div>
-            <div className="w-4 h-4 rounded-full bg-blue-250"></div>
-          </div>
+        <section className="relative z-20 flex flex-col mx-auto mt-10 lg:px-52 max-w-7xl max-lg:container">
+          <BlueLine />
           {questionData.map((question, index) => {
             return (
               <Question
@@ -90,11 +71,7 @@ export default function Questions() {
               />
             );
           })}
-          <div className="flex-center">
-            <div className="w-4 h-4 rounded-full bg-blue-250"></div>
-            <div className="w-full h-1 bg-blue-250"></div>
-            <div className="w-4 h-4 rounded-full bg-blue-250"></div>
-          </div>
+          <BlueLine />
         </section>
       </section>
       <div className="-mt-[13rem] lg:-mt-[14rem]">
@@ -135,7 +112,9 @@ function Question({ title, text, isBorder = true, index }) {
     <section className={`p-4 ${isBorder ? "border-b-2 border-blue-250" : ""} `}>
       <div onClick={() => setOpen(!isOpen)} className="cursor-pointer">
         <div className="flex items-center justify-between">
-          <h4 className="text-lg font-semibold uppercase">{title}</h4>
+          <h4 className="text-md lg:text-lg font-semibold uppercase">
+            {title}
+          </h4>
           <motion.div
             animate={isOpen ? "open" : "closed"}
             variants={iconVariants}
@@ -160,7 +139,7 @@ function Question({ title, text, isBorder = true, index }) {
                 exit={{
                   opacity: 0,
                 }}
-                className="mt-1"
+                className="mt-1 max-lg:text-sm"
               >
                 {text}
               </motion.p>
