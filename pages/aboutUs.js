6e7c84d1ab67c30d92260efb1ef2c57f84ff3a28
StR0vErBG <xcreaw1@abv.bg>
2022-11-12 18:34:42 +0300

    page init

---
+++ pages/aboutUs.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function aboutUs() {
+  return <div>aboutUs</div>;
+}
---
+++ pages/contactUs.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function contactUs() {
+  return <div>contactUs</div>;
+}
