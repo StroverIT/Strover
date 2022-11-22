cb14187c808e3da9dcbf6bddf470ace63c778785
StR0vErBG <xcreaw1@abv.bg>
2022-11-22 08:28:42 +0300

    adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
+/* eslint-disable @next/next/inline-script-id */
+
 import "../styles/globals.css";
 import Layout from "../components/layouts/Layout";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {" "}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname,
+    });
+`}
+      </Script>
       <Head>
         <link rel="icon" href="/logos/web-spider.svg" />
       </Head>
