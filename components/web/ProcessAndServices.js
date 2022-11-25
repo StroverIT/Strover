11fcd57f8abf42c9e60189918b10e36211254cac
StR0vErBG <xcreaw1@abv.bg>
2022-11-25 23:52:18 +0300

    removing spacing

--- components/web/ProcessAndServices.js
+++ components/web/ProcessAndServices.js
@@ -6,7 +6,7 @@ import FadeFromBottom from "../layouts/animations/onView/FadeFromBottom";
 
 export default function ProcessAndServices() {
   return (
-    <section className="mt-20 max-lg:container md:mt-80" id="services">
+    <section className="mt-20 max-lg:container md:mt-20" id="services">
       <section className="flex-col text-center flex-center">
         <div className="font-semibold uppercase text-blue-50 flex-center">
           <HeaderSpider title="Процеса и услугите ни" size="text-4xl" />
