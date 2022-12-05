03d4a307549350bc79e031618e5375a1d1101133
StR0vErBG <xcreaw1@abv.bg>
2022-12-05 20:20:13 +0200

    chaning to https

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "http://strover.bg",
+  siteUrl: "https://strover.bg",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
