ac95f272cdcb5f108f939b7375b4bd1fc9b7fea0
StR0vErBG <xcreaw1@abv.bg>
2022-12-08 11:32:31 +0200

    adding party town

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @next/next/inline-script-id */
 
 import "../styles/globals.css";
@@ -5,35 +6,12 @@ import Layout from "../components/layouts/Layout";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 
+import { Partytown } from "@builder.io/partytown/react";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {" "}
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script strategy="lazyOnload">
-        {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-    page_path: window.location.pathname,
-    });
-`}
-      </Script>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-   })(window,document,'script','dataLayer',${process.env.GTM_ID});
-      `}
-      </Script>
       <Head>
         <title>Strover</title>
         <link rel="icon" href="/logos/web-spider.svg" />
@@ -52,6 +30,35 @@ function MyApp({ Component, pageProps }) {
         />
         <meta property="og:url" content="https://strover.bg/" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Partytown debug={true} forward={["dataLayer.push"]} />
+        <script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          type="text/partytown"
+          async
+        />
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname,
+    });`,
+          }}
+        />
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer',${process.env.GTM_ID});
+               `,
+          }}
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
