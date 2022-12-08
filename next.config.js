8fb4be06223f74473ecec74ddb8691717352d2b4
StR0vErBG <xcreaw1@abv.bg>
2022-12-08 12:17:39 +0200

    changing to old methods

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    nextScriptWorkers: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,42 @@
-/* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @next/next/inline-script-id */
 
 import "../styles/globals.css";
 import Layout from "../components/layouts/Layout";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
-
-import { Partytown } from "@builder.io/partytown/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {" "}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname,
+    });
+`}
+      </Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+   })(window,document,'script','dataLayer',${process.env.GTM_ID});
+      `}
+      </Script>
       <Head>
         <title>Strover</title>
-        <Partytown debug={true} forward={["dataLayer.push"]} />
-
         <link rel="icon" href="/logos/web-spider.svg" />
         <meta
           name="description"
@@ -32,34 +53,6 @@ function MyApp({ Component, pageProps }) {
         />
         <meta property="og:url" content="https://strover.bg/" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          type="text/partytown"
-          async
-        />
-        <script
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-    page_path: window.location.pathname,
-    });`,
-          }}
-        />
-        <script
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer',${process.env.GTM_ID});
-               `,
-          }}
-        />
       </Head>
       <Layout>
         <Component {...pageProps} />
