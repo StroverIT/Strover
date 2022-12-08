dac001cdeaa8a5864803139a9d2be8773974c42b
StR0vErBG <xcreaw1@abv.bg>
2022-12-08 12:25:03 +0200

    returning Google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,30 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {" "}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname,
+    });
+`}
+      </Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+   })(window,document,'script','dataLayer',${process.env.GTM_ID});
+      `}
+      </Script>
       <Head>
         <title>Strover</title>
         <link rel="icon" href="/logos/web-spider.svg" />
