bf2de72fdd2ea5fc8fa5ca5af3adf927e59f8010
StR0vErBG <xcreaw1@abv.bg>
2022-12-12 09:47:22 +0200

    fixing and styling

--- pages/web/ecommerce.js
+++ pages/web/ecommerce.js
@@ -34,11 +34,10 @@ const Ecommerce = () => {
             <section className="flex-col pt-6 flex-center">
               <section className="max-w-2xl">
                 <p className="">
-                  Доста често хората си мислят, че просто трябва да има сайт
-                  където да са показани продуктите и клиентите да имат
-                  възможността да си ги поръчат, но това не е напълно вярно. Зад
-                  успешен онлайн магазин (e-commerce), стоят много фактори,
-                  като:
+                  Често хората си мислят, че просто трябва да имат сайт където
+                  да са показани продуктите и клиентите да имат възможността да
+                  си ги поръчат, но това не е напълно вярно. Зад успешен онлайн
+                  магазин (e-commerce), стоят много фактори, като:
                 </p>
                 <ul className="ml-5 list-disc">
                   <li>Маркетингова страгетия</li>
@@ -47,8 +46,8 @@ const Ecommerce = () => {
                   <li>Добре направени &quot;Call to auction&quot; бутони</li>
                 </ul>
                 <p className="pt-2">
-                  При нас не само ще Ви изработим уникален дизайн, но сайт,
-                  който може да продавам сам по себе си.
+                  При нас не само ще Ви изработим уникален дизайн, а сайт който
+                  може да продава сам по себе си.
                 </p>
               </section>
             </section>
@@ -107,26 +106,54 @@ const Ecommerce = () => {
                 />
 
                 <Ul>
-                  <li>500 продукта</li>
-
+                  <li className="">
+                    <span className="text-green-200">500</span> продукта
+                  </li>
                   <li>Уникален дизайн</li>
                   <li>Мобилна Версия</li>
-                  <li>Онлайн плащания</li>
-
-                  <li>5 и-мейл акаунта</li>
-                  <li>Админ Панел</li>
-                  <li>Възможност за многоезичност</li>
-
                   <li>SEO филтри</li>
                   <li>Facebook чат</li>
-
-                  <li>Безплатен хост за 6 месеца</li>
-                  <li>Следене на трафика от потребители</li>
-                  <li>Индексиране във всички търсачки</li>
-                  <li>Безплатен GDPR</li>
+                  <li>
+                    <span className="text-green-200">5</span> и-мейл акаунта
+                  </li>
+                  <li>Админ Панел</li>
                   <li>Безплатен Facebook conversion API</li>
                   <li>Безплатни юридически текстове</li>
                 </Ul>
+                <ul className="ml-5">
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Онлайн
+                    плащания
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Възможност за
+                    многоезичност
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Безплатен
+                    хост за 6 месеца
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Следене на
+                    трафика от потребители
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Индексиране
+                    във всички търсачки
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Безплатен
+                    GDPR
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Безплатен
+                    Facebook conversion API
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Безплатни
+                    юридически текстове
+                  </li>
+                </ul>
               </Container>
               <Container>
                 <Title>
@@ -147,31 +174,47 @@ const Ecommerce = () => {
                 />
 
                 <Ul>
-                  <li>1000 продукта</li>
+                  <li className="">
+                    <span className="text-green-200">1000</span> продукта
+                  </li>
                   <li>Уникален дизайн</li>
-
-                  <li>Интеграция с ЕКОНТ / СПИДИ</li>
-                  <li>Онлайн плащания</li>
-
                   <li>Мобилна Версия</li>
-                  <li>10 и-мейл акаунта</li>
-                  <li>Възможност за многоезичност</li>
-
                   <li>SEO филтри</li>
                   <li>Facebook чат</li>
-
+                  <li>
+                    <span className="text-green-200">10</span> и-мейл акаунта
+                  </li>
                   <li>Админ Панел</li>
+                  <li>Безплатен Facebook conversion API</li>
+                  <li>Безплатни юридически текстове</li>
 
-                  <li>Следене и отчинате на продажбите</li>
-                  <li>имплементираме възможност за събиране на email адреси</li>
+                  <li>Онлайн плащания</li>
+                  <li>Възможност за многоезичност</li>
+                  <li>Безплатен хост за 1 година</li>
                   <li>Следене на трафика от потребители</li>
                   <li>Индексиране във всички търсачки</li>
-                  <li>Безплатен хост за 1 година</li>
-                  <li>Безплатен SSL Сертификат</li>
                   <li>Безплатен GDPR</li>
                   <li>Безплатен Facebook conversion API</li>
                   <li>Безплатни юридически текстове</li>
                 </Ul>
+                <ul className="ml-5 ">
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Интеграция с
+                    ЕКОНТ и/или СПИДИ
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Следене и
+                    отчинате на продажбите
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>
+                    Възможност за събиране на email адреси с цел email marketing
+                  </li>
+                  <li>
+                    <span className="text-green-200 pr-2">+ </span>Безплатен SSL
+                    Сертификат
+                  </li>
+                </ul>
               </Container>
             </section>
           </section>
