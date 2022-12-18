a87b7a57045aa5c75605e096228e02d6be3388dc
StR0vErBG <xcreaw1@abv.bg>
2022-12-18 13:01:03 +0200

    fixing styling

--- components/promotions/newYear/ChristmasButton.js
+++ components/promotions/newYear/ChristmasButton.js
@@ -4,7 +4,7 @@ import style from "./christmasButton.module.css";
 
 const ChristmasButton = ({ onClick, isLoading }) => {
   return (
-    <div className={`${style.centerer} w-full mt-2 md:mt-0`}>
+    <div className={`${style.centerer} w-full mt-2 md:mt-0 cursor-pointer`}>
       <a
         className={`${style.button} max-md:w-full text-center py-2 md:px-12 text-lg md:py-5 md:text-2xl `}
         onClick={onClick}
