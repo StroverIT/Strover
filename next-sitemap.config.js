87c2595c7d45d1cf588ead976285692319960c34
StR0vErBG <xcreaw1@abv.bg>
2023-01-09 19:31:40 +0200

    adding links not to crawl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,13 @@
 module.exports = {
   siteUrl: "https://strover.bg",
   generateRobotsTxt: true, // (optional)
+  exclude: [
+    "/terms-and-conditions",
+    "/privacy-policy",
+    "/web/thanks",
+    "/web/thanks-christmas",
+    "/cookie-policy",
+    "/web/promotions",
+  ],
   // ...other options
 };
