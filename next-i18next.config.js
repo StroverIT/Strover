51d964b45bed3da7f1c09f605b3f7c4564766f5a
StR0vErBG <xcreaw1@abv.bg>
2023-03-04 00:47:29 +0200

    fixing config and everything must work

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,24 +1,51 @@
 const path = require("path");
+const NextI18Next = require("next-i18next");
 
 const LANGUAGES = ["bg", "en"];
 const DEFAULT_LANGUAGE = "bg";
 
 // if it is the server, then the full path, if the client, then the relative path.
-// const localePath =
-//   typeof window === "undefined"
-//     ? path.resolve("public", "translation")
-//     : "/public/translation";
+const localePath =
+  typeof window === "undefined"
+    ? path.resolve("public", "locales")
+    : "/public/locales";
 
+// module.exports = {
+//   i18n: {
+//     defaultLocale: DEFAULT_LANGUAGE,
+//     locales: LANGUAGES,
+//     fallbackLng: LANGUAGES,
+//     nsSeparator: "::",
+//     keySeparator: "::",
+//     // How to use libraries for i18next like LanguageDetector
+//     use: [require("i18next-intervalplural-postprocessor")],
+//     serializeConfig: false,
+//   },
+//   localePath: localePath,
+// };
 module.exports = {
+  browserLanguageDetection: true,
+  serverLanguageDetection: true,
+  defaultNS: "common",
+  defaultLanguage: "bg",
+  otherLanguages: ["de, es, fr"],
+  localeStructure: "{{lng}}/{{ns}}",
+  localeExtension: "json",
+  localePath: "locales",
+
+  localeSubpaths: {
+    bg: "bg",
+    en: "en",
+  },
   i18n: {
     defaultLocale: DEFAULT_LANGUAGE,
     locales: LANGUAGES,
-    fallbackLng: LANGUAGES,
-    nsSeparator: "::",
-    keySeparator: "::",
+    // fallbackLng: LANGUAGES,
+    // nsSeparator: "::",
+    // keySeparator: "::",
     // How to use libraries for i18next like LanguageDetector
-    use: [require("i18next-intervalplural-postprocessor")],
-    serializeConfig: false,
+    // use: [require("i18next-intervalplural-postprocessor")],
+    // serializeConfig: false,
   },
-  // localePath: localePath,
+  localePath: localePath,
 };
