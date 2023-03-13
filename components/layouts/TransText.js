d2fa358bd899d2951df6dda6967dc3bb075e12a4
StR0vErBG <xcreaw1@abv.bg>
2023-03-13 21:26:51 +0200

    removing console.log

--- components/layouts/TransText.js
+++ components/layouts/TransText.js
@@ -3,7 +3,6 @@ import { useTranslation } from "next-i18next";
 
 const TransText = ({ children, trans = "home" }) => {
   const { t } = useTranslation(trans);
-  console.log(typeof children);
   const text = children.toString();
   return t(text);
 };
